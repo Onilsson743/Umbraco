64af37aefb0dfdf0201f5db0069cede7a30bc580
Oscar Nilsson <o.nilsson743@gmail.com>
2023-09-12 10:44:08 +0200

    First Commit

--- wwwroot/scripts/script.js
+++ wwwroot/scripts/script.js
@@ -1,2 +1,14 @@
+function openMobileMenu() {
+    const menu = document.getElementById("navigation-dropdown-menu");
+    
+    if (!menu.classList.contains("mobile-menu-open")) {
+        menu.classList.add("mobile-menu-open");
+    } else {
+        menu.classList.remove("mobile-menu-open");
 
-                        
+    }
+}
+
+function Test(item) {
+    console.log(item)
+}
