f852b7acf0b403e178e44d7d85f00af28f78630d
unknown <o.nilsson743@gmail.com>
2023-10-05 11:02:04 +0200

    Added scroll function to eployee cards section

--- wwwroot/scripts/script.js
+++ wwwroot/scripts/script.js
@@ -14,10 +14,13 @@ function closePopUpWindow() {
     popupwindow.classList.add("d-none");
 }
 
-function scrollToPosition(amount, item) {
+function scrollArticleRowToPosition(amount, id) {
     var scrollDiv = document.getElementById("scrollable-article-div")
+    var idName = "articleBarScrollTo" + id
+    var thisitem = document.getElementById(idName)
 
-    for (i = 0; i < 4; i++) {
+
+    for (i = 1; i < 5; i++) {
         var scrollButtonId = "articleBarScrollTo" + i
         var scollButton = document.getElementById(scrollButtonId)
         try {
@@ -31,8 +34,32 @@ function scrollToPosition(amount, item) {
         behavior: "smooth",
     });
 
-    item.classList.add("active")
+    thisitem.classList.add("active")
     
+}
+
+function scrollEmployeeRowToPosition(amount, id) {
+    var scrollDiv = document.getElementById("scrollable-employee-cards-div")
+    var idName = "employeesBarScrollTo" + id
+    var thisitem = document.getElementById(idName)
+
+
+    for (i = 1; i < 5; i++) {
+        var scrollButtonId = "employeesBarScrollTo" + i
+        var scollButton = document.getElementById(scrollButtonId)
+        try {
+            scollButton.classList.remove("active")
+        } catch (e) { }
+
+    }
+
+    scrollDiv.scrollTo({
+        left: amount,
+        behavior: "smooth",
+    });
+
+    thisitem.classList.add("active")
+
 }
 
 
